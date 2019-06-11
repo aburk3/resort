31e0987a3d90d75d6ef2e2b3c500be3b68e7db14
aburk3 <aburk3@gmail.com>
2019-06-11 06:00:55 -0500

    Added react-icons with title and info properties

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,41 @@
 import React, { Component } from "react";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 import Title from "./Title";
 
 export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "free cocktails",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, rem."
+      },
+      {
+        icon: <FaHiking />,
+        title: "Endless Hiking",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, rem."
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "Free shuttle",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, rem."
+      },
+      {
+        icon: <FaBeer />,
+        title: "Best Beer",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, rem."
+      }
+    ]
+  };
   render() {
     return (
-      <div>
+      <section className="services">
         <Title title="services" />
-      </div>
+      </section>
     );
   }
 }
