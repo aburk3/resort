663d6f6743befd6f70e3c056110eefb2cd7d8979
aburk3 <aburk3@gmail.com>
2019-06-11 05:07:16 -0500

    Added state and toggle method to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,25 @@ import { faAlignRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
   render() {
-    return <div>Hello from navbar</div>;
+    return (
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="Beach Resort" />
+            </Link>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
