b8df7a52cf23aa3aa62fca8969c07e584e752bd1
aburk3 <aburk3@gmail.com>
2019-06-14 07:10:58 -0500

    Added loading gif

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from "./Loading";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    return <div>hello from featured rooms</div>;
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
+    return (
+      <div>
+        hello from featured rooms
+        <Loading />;
+      </div>
+    );
   }
 }
