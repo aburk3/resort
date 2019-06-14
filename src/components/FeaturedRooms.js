4d610777cd5469af19b39e50eba0c1131e745f2c
aburk3 <aburk3@gmail.com>
2019-06-14 06:37:16 -0500

    Hooked up the context api to FeaturedRooms component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
+import { RoomContext } from "../context";
 
 export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
   render() {
-    return <div>hello from featured rooms</div>;
+    const { name, greeting } = this.context;
+
+    return <div>hello from featured rooms {name}</div>;
   }
 }
