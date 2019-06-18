6a5d1c48aad9812b15cc662115c1c49de8c75342
aburk3 <aburk3@gmail.com>
2019-06-18 05:28:06 -0500

    Corrected state values, set up handleChange event method

--- src/context.js
+++ src/context.js
@@ -8,26 +8,32 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: true
+    loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   };
 
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
+
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: true,
-      type: "all",
-      capacity: 1,
-      price: 0,
-      minPrice: 0,
-      maxPrice: 0,
-      minSize: 0,
-      maxSize: 0,
-      breakfast: false,
-      pets: false
+      price: maxPrice,
+      maxPrice,
+      maxSize
     });
   }
 
@@ -46,6 +52,12 @@ class RoomProvider extends Component {
     let tempRooms = [...this.state.rooms];
     const room = tempRooms.find(room => room.slug === slug);
     return room;
+  };
+
+  handleChange = event => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
   };
 
   render() {
