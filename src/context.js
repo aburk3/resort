a065c30d08358ff7cb608cd66aabcad934594bca
aburk3 <aburk3@gmail.com>
2019-06-18 06:09:52 -0500

    Modified handleChange method

--- src/context.js
+++ src/context.js
@@ -55,9 +55,15 @@ class RoomProvider extends Component {
   };
 
   handleChange = event => {
-    const type = event.target.type;
+    const target = event.target;
+    const value = event.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value;
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
   };
 
   filterRooms = () => {
